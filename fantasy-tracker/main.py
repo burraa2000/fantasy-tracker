fd1647b79193226897e4fd7b91b14393f025a109
Eric Ngeo <burraa2000@gmail.com>
2013-09-29 12:05:00 -0400

    initial commit

---
+++ fantasy-tracker/main.py
@@ -0,0 +1,8 @@
+import ConfigParser
+
+
+if __name__ =='__main__':
+    print "Starting your server"
+    config_parser=ConfigParser.ConfigParser()
+    config_parser.read("init.cfg")
+    
---
+++ fantasy-tracker/yahoo_oauth_handler.py
@@ -0,0 +1,19 @@
+'''
+Created on Sep 29, 2013
+
+@author: eric
+'''
+
+import constants
+
+class YahooHandler(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,params):
+        '''
+        Constructor
+        '''
+        
